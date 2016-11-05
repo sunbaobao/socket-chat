da5c6a84bcd9b1d9a8e89e7784b10e4bbb9b535b
sunbaobao <aa@c>
2016-11-05 16:19:26 +0800

    4010

--- index.js
+++ index.js
@@ -40,6 +40,6 @@ io.on('connection', function(socket){
         }
     });
 });
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+http.listen(4010, function(){
+    console.log('listening on *:4010');
 });
